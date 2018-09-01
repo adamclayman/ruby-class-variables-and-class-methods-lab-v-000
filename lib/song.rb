af523b998a19f50c442c03ec97be112157048b74
Adam Clayman <adam@clayman.org>
2018-09-01 02:22:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-=begin
+
     @@count += 1
     @@artist << artist if @@artists.include?(artist) == nil
     if @@genres.include?(genre) == nil
@@ -19,7 +19,6 @@ class Song
     else
       GENRES[genre] += 1
     end
-=end
   end
 
   def self.count
