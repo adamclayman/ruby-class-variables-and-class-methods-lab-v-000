3a88fb2a9fd45397aedf4032e858fcaedc406b67
Adam Clayman <adam@clayman.org>
2018-09-01 02:19:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,14 @@ class Song
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
-    @@genres << genre if @@genres.include?(genre) == nil
+    @@count += 1
+    @@artist << artist if @@artists.include?(artist) == nil
+    if @@genres.include?(genre) == nil
+      @@genres << genre
+      @@genre_count[genre] += 1
+    end
+    
+    if @@genres_count.keys.include?(genre) == nil
   end
 
   def self.count
